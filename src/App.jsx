c6c09b7fd16c8b3d202c932e6fd26d9ad31f4678
Ezra M/K <97867117+ezoltech@users.noreply.github.com>
2025-10-08 15:36:04 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen">
+        <h2> hello world</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
