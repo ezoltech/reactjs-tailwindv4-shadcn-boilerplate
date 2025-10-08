5d543b01790a0ba7e790e4842662789b8c9c5447
ezoltech <ezramamo13@gmail.com>
2025-10-08 16:47:00 +0300

    adding readme.md

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { Button } from "./components/ui/button";
 function App() {
   return (
     <>
-      <div className="flex items-center justify-center h-screen">
+      <div className="flex flex-col gap-2 items-center justify-center h-screen">
         <h2> hello world</h2>
+        <Button>Test Button</Button>
       </div>
     </>
   );
